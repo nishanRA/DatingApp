69d8337c0842f7ba6cb72d33b20420eb8c665205
nishanRA <nishan.adhikari0493@gmail.com>
2019-09-08 18:09:08 +0530

    End of Section 8 Extending Api

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -7,11 +7,11 @@ namespace DatingApp.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options)
         :base(options)
-        {
-            
+        {            
         }
 
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
     }
 }
